271f1fa98ef29b77258670890f5ef9d4ef8f88cb
Kamonchanok Ketsiritrakul <kmlchnkkessiritrakul121@gmail.com>
2025-09-28 14:25:09 +0700

    เพิ่มไฟล์ฐานข้อมูล

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,25 +1,3 @@
-// // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
-// import "controllers"
-
-// // สำหรับ Rails UJS (ใช้ link_to method: :delete)
-// import Rails from "@rails/ujs"
-
-// console.log("Start");
-// Rails.start()
-// console.log("rails run");
-
-// import "bootstrap"
-// import * as bootstrap from "bootstrap"
-
-// import "jquery"   // เพิ่ม jQuery
-// Load Rails UJS for method: :delete
-import Rails from "@rails/ujs"
-Rails.start()
-
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-// Optional
-import "bootstrap"
-import "jquery"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,20 +1,7 @@
-# # Pin npm packages by running ./bin/importmap
-
-# pin "application"
-# pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
-# pin "@hotwired/turbo-rails", to: "turbo.min.js"
-# pin "@hotwired/stimulus", to: "stimulus.min.js"
-# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-# pin_all_from "app/javascript/controllers", under: "controllers"
-# pin "bootstrap", to: "bootstrap.bundle.min.js"
-# pin "jquery", to: "jquery.js" # @3.7.1
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@rails/ujs", to: "rails-ujs.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.bundle.min.js"
-pin "jquery", to: "jquery.js"
